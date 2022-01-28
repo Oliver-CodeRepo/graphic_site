e94da87c8b730e952a13769b0ef6197e8496ade0
Oliver-CodeRepo <olivermulaku@gmail.com>
2022-01-28 03:18:31 +0300

    migration

--- base/models.py
+++ base/models.py
@@ -3,11 +3,11 @@ from  embed_video.fields  import  EmbedVideoField
 import string
 import random
 
-from django.utils.text import slugify
+# from django.utils.text import slugify
 
 
-def rand_slug():
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+# def rand_slug():
+#     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
 
 
 
@@ -17,13 +17,7 @@ class Video(models.Model):
     title = models.CharField(max_length=200)
     video = EmbedVideoField()
     details = models.TextField()
-    # slug = models.SlugField(max_length=255, unique=True)    
-    
-    # def save(self, *args, **kwargs):
-    #     super(Video, self).save(*args, **kwargs)
-    #     self.slug = slugify(rand_slug() + "-" + self.title)
-    #     super(Video, self).save(*args, **kwargs)
-    
+        
     def __str__(self):
         return self.title if self.title else 'video_{}'.format(self.id)
     
